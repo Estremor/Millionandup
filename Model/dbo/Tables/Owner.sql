40cca1552e780922ead2959383c792680bfa9acb
Alexextremor <alexextremor@gmail.com>
2024-01-15 22:08:28 -0500

    squema models million

---
+++ Model/dbo/Tables/Owner.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Owner] (
+    [IdOwner]  UNIQUEIDENTIFIER NOT NULL,
+    [Name]     VARCHAR (200)    NOT NULL,
+    [Address]  VARCHAR (500)    NOT NULL,
+    [Document] NVARCHAR (MAX)   NOT NULL,
+    [Photo]    IMAGE            NOT NULL,
+    [Birthday] DATETIME         NOT NULL,
+    CONSTRAINT [PK_Owner] PRIMARY KEY CLUSTERED ([IdOwner] ASC)
+);
+
